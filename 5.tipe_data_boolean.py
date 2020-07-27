9206c8ddca93cf0e7ac01bcb175c3c79af9a64cd
Setyo Dwi Pratama <setyo_dwipratama@yahoo.com>
2020-07-27 23:25:25 +0700

    Add files via upload

---
+++ 5.tipe_data_boolean.py
@@ -0,0 +1,18 @@
+# TIPE DATA BOOLEAN
+print("===============TIPE DATA BOOLEAN===============")
+# Tipe data boolean hanya bisa diisi dengan salah satu dari dua nilai yaitu True dan False. tipe data ini biasanya dipakai untuk percabangan kode program atau untuk memutuskan apa yang mesti dijalankan ketika sebuah kondisi terjadi.
+
+benar = True
+salah = False
+
+print(benar, salah)
+
+a = 1 < 2
+b = 1 > 2
+c = 1 == 2
+
+print("1 < 2 = ", a)
+print("1 > 2 = ", b)
+print("1 == 2 = ", c)
+
+# kasus tipe data boolean akan dibahas lebih lanjut ketika dalam membuat fungsi dan pengkondisian
