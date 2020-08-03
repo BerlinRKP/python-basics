d14dcb180eb7bb8f20ce2c68ae811dea87b7e5ee
Setyo Dwi Pratama <setyo_dwipratama@yahoo.com>
2020-08-03 20:43:50 +0700

    Add files via upload

---
+++ 10.jenis_operator.py
@@ -0,0 +1,40 @@
+# JENIS OPERATOR
+print("===============JENIS OPERATOR===============")
+
+# 1. OPERATOR ARITMATIKA
+# +, -, *, /, //, %, **
+
+# 2. OPERATOR PERBANDINGAN/RELASIONAL
+# ==, !=, >, <, >=, <=
+
+# 3. OPERATOR LOGIKA
+# and, or, not
+
+# 4. OPERATOR BITWISE
+# Operator Bitwise merupakan operator khusus untuk menangani operasi logika bilangan biner dalam bentuk bit (bilangan biner : 0 dan 1)
+# & = and, | = or, ^ = xor, ~ = not, << = left shift, >> = right shift
+
+# 5. OPERATOR ASSIGNMENT
+# Operator assignment merupakan operator untuk memasukkan suatu nilai ke dalam variable. dalam bahasa python, operator assignment menggunakan tanda sama dengan (=)
+
+# Operator assignment juga memiliki variasi penulisan yang disebut dengan operator assignment gabungan (compound assignment). operator assignment gabungan ini merupakan cara penulisan singkat sebuah operator yang digabung dengan operator lainnya seperti aritmatika dan bitwise
+# += contohnya a += b yang artinya a = a + b
+# -= contohnya a -= b yang artinya a = a - b
+# *= contohnya a *= b yang artinya a = a * b
+# /= contohnya a /= b yang artinya a = a / b
+# %= contohnya a %= b yang artinya a = a % b
+# &= contohnya a &= b yang artinya a = a & b
+# |= contohnya a |= b yang artinya a = a | b
+# ^= contohnya a ^= b yang artinya a = a ^ b
+# <<= contohnya a <<= b yang artinya a = a << b
+# >>= contohnya a >>= b yang artinya a = a >> b
+
+# 6. OPERATOR IDENTITAS (IDENTITY OPERATORS)
+# Operator identitas merupakan operator yang bisa dipakai untuk memeriksa apakah nilai sebuah variabel ada di tempat yang sama atau tidak.
+# is = bernilai True jika kedua operand atau nilai merujuk ke object yang sama dan berisi nilai yang sama
+# is not bernilai True juka kedua oprand atau nilai merujuk ke object yang tidak sama
+
+# 7. OPERATOR KEANGGOTAAN (MEMBERSHIP OPERASTORS)
+# Operator keanggotaan merupakan operator yang dipakai untuk memeriksa apakah suatu nilai ada di dalam sebuah himpunan atau tidak. himpunyan yang dimaksud terdiri dari tipe data "berbentuk array" seperti string, list, tuple, set, dan dictonary.
+# in = bernilai True jika nilai yang dicari ada di dalam himpunan
+# not in = bernilai True jika nilai yang dicari tidak ada di dalam himpunan
