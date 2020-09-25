c9f5654a990ce40ef71863d119d1963a56d600ca
Setyo Dwi Pratama <setyo_dwipratama@yahoo.com>
2020-09-25 19:21:18 +0700

    Add files via upload

---
+++ 12.oper_perbandingan.py
@@ -0,0 +1,19 @@
+# OPERATOR PERBANDINGAN
+print("===============OPERATOR PERBANDINGAN===============")
+# Operator Perbandingan merupakan operator untuk membandingkan suatu nilai. hasil dari perbandingan nya hanya akan mengeluarkan boolean yaitu True dan False
+# Operator perbandingan ini biasa dipakai dalam proses pengambilan keputusan atau percabangan pada kode program
+# Operator perbandingan selain digunakan untuk data number bisa juga untuk kasus data string
+
+
+x = 1
+y = 10
+print("x =", x)
+print("y =", y)
+print("\n")
+
+print("x == y hasilnya", x == y)
+print("x != y hasilnya", x != y)
+print("x > y hasilnya", x > y)
+print("x < y hasilnya", x < y)
+print("x >= y hasilnya", x >= y)
+print("x <= y hasilnya", x <= y)
