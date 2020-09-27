4fcf60350689273652b70745afe6ecb87127bd2e
Setyo Dwi Pratama <setyo_dwipratama@yahoo.com>
2020-09-27 11:57:13 +0700

    Add files via upload

---
+++ 15.oper_assignment.py
@@ -0,0 +1,23 @@
+# OPERATOR ASSIGNMENT
+print("===============OPERATOR ASSIGNMENT===============")
+# Operator assignment adalah operator untuk memasukkan suatu nilai ke dalam variabel. Dalam bahasa Python, operator assignment menggunakan tanda sama dengan (=), operator ini juga disebut operator penugasan.
+
+a = 4
+b = 1
+print(a=b)
+
+# Operator Assigment Gabungan
+print("===============OPERATOR ASSIGNMENT GABUNGAN===============")
+# Operator assignment juga memiliki variasi penulisan yang disebut sebagai operator assignment gabungan (compound assignment). Operator assignment gabungan adalah cara penulisan singkat operator assignment yang digabung dengan dengan operator lain. Dalam bahasa Python, operator assignment gabungan ini terdiri dari operator assignment dengan operator lain seperti operator aritmatika dan bitwise.
+# += : a+=b : a=a+b
+# -= : a-=b : a=a-b
+# *= : a*=b : a=a*b
+# /= : a/=b : a=a/b
+# //= : a//=b : a=a//b
+# %= : a%=b : a=a%b
+# **= : a**=b : a=a**b
+# &= : a&=b : a=a&b
+# |= : a|=b : a=a|b
+# ^= : a^=b : a=a^b
+# <<= : a<<=b : a=a<<b
+# >>= : a>>=b : a=a>>b
